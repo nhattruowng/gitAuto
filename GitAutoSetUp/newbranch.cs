e356c6da48c249e0fd2dc59462d64ea5d95df6bd
nhattruowng <ltn04098@gmail.com>
2024-10-25 16:10:17 +0700

    ahdvak

--- GitAutoSetUp/newbranch.cs
+++ GitAutoSetUp/newbranch.cs
@@ -17,14 +17,16 @@ namespace GitAutoSetUp
 
         private void btnluu_Click(object sender, EventArgs e)
         {
-            textBox2.Text += GitHelper.SaveCredentials(username: txtname.Text, password: txtpass.Text);
+            textBox2.Text += GitHelper.SaveCredentials(username: txtname.Text, email:txtpass.Text, token:txttoken.Text );
         }
 
         private void newbranch_Load(object sender, EventArgs e)
         {
             var inf = GitHelper.LoadCredentials();
+
             txtname.Text = inf.Username != null ? inf.Username : "N/A";
             txtpass.Text = inf.Password != null ? inf.Password : "N/A";
+            txttoken.Text = inf.token != null ? inf.token : "N/A";
         }
     }
 }
